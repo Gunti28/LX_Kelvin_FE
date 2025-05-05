77962dfdb8dc4605a059f094a6df28a53359bcd2
Sulthan000 <syedsulthanbasha@srnritsolutions.com>
2025-05-05 10:47:07 +0530

    Implemented Landing Page (First Code)

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Navbaromponent from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+<Navbaromponent/>
+    </div>
+  );
+}
+
+export default App;
