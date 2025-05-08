533fefe6a6e0c05335275648778f9377ba79c378
Suneel0028 <suneel0028@gmail.com>
2025-05-08 15:32:31 +0530

    category changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import Navbaromponent from './components/Navbar';
+// src/App.js
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import HomePage from './components/HomePage'; // update the path if necessar
+import HomePage from './pages/Categories';
 
 function App() {
   return (
-    <div className="App">
-<Navbaromponent/>
-    </div>
-  );
+    <>
+    <HomePage />
+    </>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //     {/* Add more routes here if needed */}
+    //   </Routes>
+    // </Router>
+);
 }
 
 export default App;
+
+
